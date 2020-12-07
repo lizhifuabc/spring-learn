c61bcb9585d33fc64681204914a6d9e1fd53c24b
lizhifu <lizhifuabc@163.com>
2020-12-07 16:40:34 +0800

    spring boot druid

---
+++ spring-boot-druid/src/main/java/com/boot/druid/SpringBootDruidApplication.java
@@ -0,0 +1,17 @@
+package com.boot.druid;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动
+ * <p>文档：https://github.com/alibaba/druid/tree/master/druid-spring-boot-starter</p>
+ * @author lizhifu
+ * @date 2020/12/4
+ */
+@SpringBootApplication
+public class SpringBootDruidApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootDruidApplication.class);
+    }
+}
