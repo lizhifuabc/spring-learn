b8d4bd6c681a8db0d3a6cd3bb0211e0889ff056e
lizhifu <lizhifuabc@163.com>
2020-12-10 11:40:34 +0800

    添加描述

--- spring-boot-zk/src/main/java/com/boot/zk/SpringBootZkApplication.java
+++ spring-boot-zk/src/main/java/com/boot/zk/SpringBootZkApplication.java
@@ -5,6 +5,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
  * 启动程序
+ * <p>
+ *     zk 分布式锁，其实可以做的比较简单，就是某个节点尝试创建临时 znode，
+ *     此时创建成功了就获取了这个锁；这个时候别的客户端来创建锁会失败
+ *     ，只能注册个监听器监听这个锁。释放锁就是删除这个 znode，一旦释放掉就会通知客户端，
+ *     然后有一个等待着的客户端就可以再次重新加锁。
+ * </p>
  * 依赖maven：https://github.com/apache/curator/tree/master/curator-examples/src/main/java
  * https://zookeeper.apache.org/
  * <li>启动zk:/zkServer.sh start</li>
