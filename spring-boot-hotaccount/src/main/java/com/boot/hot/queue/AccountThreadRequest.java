fe659d46b4c54438ecd3c987e7de69d3820289e1
lizhifu <lizhifuabc@163.com>
2020-12-22 10:00:46 +0800

    热点账户，队列提交

--- spring-boot-hotaccount/src/main/java/com/boot/hot/queue/AccountThreadRequest.java
+++ spring-boot-hotaccount/src/main/java/com/boot/hot/queue/AccountThreadRequest.java
@@ -6,6 +6,7 @@ import lombok.extern.slf4j.Slf4j;
 import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.locks.LockSupport;
 
 /**
  * 请求数据
@@ -15,8 +16,10 @@ import java.util.concurrent.LinkedBlockingQueue;
  */
 @Slf4j
 @Data
-public class AccountRequest {
-    private Long accountNo;
+public class AccountThreadRequest implements Runnable{
+    private volatile Thread writer;
+    /** 线程名称 **/
+    private final Long accountNo;
     /** 队列 **/
     private LinkedBlockingQueue<Long> queue;
     /** 队列最大 */
@@ -25,6 +28,11 @@ public class AccountRequest {
     private int interval = 40;
     /** 上一次时间 */
     private volatile long lastExeTime  = System.currentTimeMillis();;
+
+    public AccountThreadRequest(Long accountNo) {
+        this.accountNo = accountNo;
+    }
+
     /**
      * 添加数据:判断是否超时或者超过最大队列
      * offer() 方法会返回 false。因此就可以在程序中进行有效的判断！
@@ -57,5 +65,42 @@ public class AccountRequest {
      */
     private boolean canExe(){
         return queue.size() > maxSize || System.currentTimeMillis() - lastExeTime > interval;
+    }
+
+    /**
+     * 是否超时
+     */
+    public void timeOut(){
+        //超过两次提交超过时间间隔
+        if(System.currentTimeMillis() - lastExeTime >= interval){
+            start();
+        }
+    }
+    /**
+     * 当前的数据是否大于提交的条件
+     */
+    private void maxSize(){
+        if(queue.size() >= maxSize){
+            start();
+        }
+    }
+    /**
+     * 解除线程的阻塞
+     */
+    private void start(){
+        LockSupport.unpark(writer);
+    }
+    @Override
+    public void run() {
+        writer = Thread.currentThread();
+        writer.setName(String.valueOf(accountNo));
+        while (!writer.isInterrupted()){
+            while (!canExe()){
+                //如果线程没有被打断，且不达到执行的条件，则阻塞线程
+                LockSupport.park(this);
+            }
+            //执行方法
+            exe();
+        }
     }
 }
---
+++ spring-boot-hotaccount/src/main/java/com/boot/hot/queue/QueueThreadService.java
@@ -0,0 +1,58 @@
+package com.boot.hot.queue;
+
+import com.boot.hot.dao.AccountHisDao;
+import com.boot.hot.dao.param.AccountHisParam;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.Random;
+import java.util.concurrent.*;
+import java.util.concurrent.atomic.AtomicInteger;
+
+/**
+ * 队列
+ *
+ * @author lizhifu
+ * @date 2020/12/21
+ */
+public class QueueThreadService {
+    private AtomicInteger index;
+    private final AccountThreadRequest[] accountThreadRequests;
+    //防止多个线程同时执行。增加一个随机数间隔
+    private static final Random r = new Random();
+    private static final int delta = 50;
+    /** timer线程 **/
+    private static ScheduledExecutorService TIMER = new ScheduledThreadPoolExecutor(1);
+    private static ExecutorService POOL = Executors.newCachedThreadPool();
+
+    public static final ConcurrentHashMap<Long, AccountRequest> map = new ConcurrentHashMap();
+
+    public QueueThreadService(int threads,Long accountNo) {
+        this.accountThreadRequests = new AccountThreadRequest[threads];
+        if(threads > 1){
+            index = new AtomicInteger();
+        }
+        for (int i = 0; i < threads; i++) {
+            final AccountThreadRequest flushThread = new AccountThreadRequest(accountNo);
+            accountThreadRequests[i] = flushThread;
+            POOL.submit(flushThread);
+            //定时调用 timeOut()方法。
+            TIMER.scheduleAtFixedRate(flushThread::timeOut,
+                    r.nextInt(delta), flushThread.getInterval(), TimeUnit.MILLISECONDS);
+        }
+    }
+
+    /**
+     * 对 index 取模，保证多线程都能被add
+     * @param id
+     * @return
+     */
+    public boolean add(Long id){
+        int len = accountThreadRequests.length;
+        if(len == 1){
+            return accountThreadRequests[0].add(id);
+        }
+        int mod = index.incrementAndGet() % len;
+        return accountThreadRequests[mod].add(id);
+    }
+}
