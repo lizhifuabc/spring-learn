438aad2725429d44f08e01170cf6ac591a6c670c
lizhifu <lizhifuabc@163.com>
2020-12-22 14:31:09 +0800

    druid login url

--- spring-boot-druid/src/main/java/com/boot/druid/SpringBootDruidApplication.java
+++ spring-boot-druid/src/main/java/com/boot/druid/SpringBootDruidApplication.java
@@ -6,6 +6,8 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 /**
  * 启动
  * <p>文档：https://github.com/alibaba/druid/tree/master/druid-spring-boot-starter</p>
+ * <p>druid登录地址：http://localhost:8080/learn/druid/login.html</p>
+ * <p>admin/admin</p>
  * @author lizhifu
  * @date 2020/12/4
  */
