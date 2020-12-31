91bb42417f37ffc7af2da9b449d272f6d54afa1d
lizhifu <lizhifuabc@163.com>
2020-12-31 14:48:43 +0800

    RabbitMQ:启用事务或者cofirm机制

--- spring-boot-mq/spring-boot-rabbitmq/src/main/java/com/boot/rabbitmq/controller/RabbitMqTransController.java
+++ spring-boot-mq/spring-boot-rabbitmq/src/main/java/com/boot/rabbitmq/controller/RabbitMqTransController.java
@@ -1,7 +1,8 @@
 package com.boot.rabbitmq.controller;
 
-import com.mq.common.message.MessageObject;
+import com.boot.rabbitmq.trans.TransactionSender;
 import com.mq.common.constant.RabbitMqConstant;
+import com.mq.common.message.MessageObject;
 import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -10,19 +11,17 @@ import javax.annotation.Resource;
 
 /**
  * Direct
- * 创建 queue 的时候将其设置为持久化
- * 发送消息设置发送模式deliveryMode=2代表持久化消息:默认
- * 关闭自动ack，使用手动ack
+ *
  * @author lizhifu
  * @date 2020/12/17
  */
 @RestController
-public class RabbitMqDirectController {
+public class RabbitMqTransController {
     @Resource
-    private RabbitTemplate rabbitTemplate;
-    @GetMapping("/sendDirect")
+    private TransactionSender transactionSender;
+    @GetMapping("/trans")
     public String sendDirect() {
-        rabbitTemplate.convertAndSend(RabbitMqConstant.DIRECT_QUEUE, new MessageObject("direct message"));
+        transactionSender.send("RabbitMqConstant.DIRECT_QUEUE", new MessageObject("trans message"));
         return "发送成功";
     }
 }
---
+++ spring-boot-mq/spring-boot-rabbitmq/src/main/java/com/boot/rabbitmq/trans/TransactionSender.java
@@ -0,0 +1,27 @@
+package com.boot.rabbitmq.trans;
+
+import org.springframework.amqp.rabbit.core.RabbitTemplate;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+import javax.annotation.Resource;
+
+/**
+ * TransactionSender
+ * RabbitMQ 事务机制（同步），基本上吞吐量会下来，因为太耗性能。
+ * @author lizhifu
+ * @date 2020/12/31
+ */
+@Component
+public class TransactionSender {
+    @Resource
+    private RabbitTemplate rabbitTemplate;
+    @Transactional(rollbackFor = Exception.class)
+    public void send(String routingKey, Object object) {
+        // 开启事务模式 如果开启事务 需要关闭spring.rabbitmq.publisher-confirm-type=NONE
+        rabbitTemplate.setChannelTransacted(true);
+        rabbitTemplate.convertAndSend(routingKey, object);
+        //测试消息是否会回滚 此时消费者是无法获取到消息的
+        throw new RuntimeException("测试");
+    }
+}
