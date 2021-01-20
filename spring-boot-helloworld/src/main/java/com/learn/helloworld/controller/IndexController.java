3a4eef2848a503e078ad0dd13e591318396c4c57
lizhifu <lizhifuabc@163.com>
2021-01-20 16:39:37 +0800

    接口返回list

--- spring-boot-helloworld/src/main/java/com/learn/helloworld/controller/IndexController.java
+++ spring-boot-helloworld/src/main/java/com/learn/helloworld/controller/IndexController.java
@@ -1,7 +1,11 @@
 package com.learn.helloworld.controller;
 
+import com.learn.helloworld.domain.User;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * IndexController
@@ -14,5 +18,15 @@ public class IndexController {
     @GetMapping("/")
     public String helloWorld(){
         return "helloWorld";
+    }
+    @GetMapping("/user")
+    public List<User> user(){
+        List<User> list = new ArrayList<>();
+        User user = new User();
+        user.setName("小马");
+        user.setAge(18);
+        list.add(user);
+        list.add(user);
+        return list;
     }
 }
---
+++ spring-boot-helloworld/src/main/java/com/learn/helloworld/domain/User.java
@@ -0,0 +1,28 @@
+package com.learn.helloworld.domain;
+
+/**
+ * User
+ *
+ * @author lizhifu
+ * @date 2021/1/20
+ */
+public class User {
+    private String name;
+    private Integer age;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
