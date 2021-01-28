3189e3c8eadaf1bd00b098189eb88a367b068aa2
lizhifu <lizhifuabc@163.com>
2021-01-28 11:18:10 +0800

    增加map转换示例

--- spring-boot-wxmp/src/main/java/com/boot/wxmap/api/AccessTokenApi.java
+++ spring-boot-wxmp/src/main/java/com/boot/wxmap/api/AccessTokenApi.java
@@ -4,7 +4,12 @@ import com.boot.wxmap.api.accesstoken.AccessTokenParam;
 import com.boot.wxmap.base.BaseAbstractApi;
 import com.boot.wxmap.base.WxResponseEntity;
 import com.boot.wxmap.client.WxClient;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * AccessToken
@@ -19,6 +24,7 @@ public class AccessTokenApi extends BaseAbstractApi {
      * @param accessTokenParam
      * @return
      */
+    @SneakyThrows
     public WxResponseEntity getAccessToken(AccessTokenParam accessTokenParam){
         String url = "https://api.weixin.qq.com/cgi-bin/token?grant_type=client_credential&appid="+
                 accessTokenParam.getAppId()+"&secret="+accessTokenParam.getSecret();
@@ -30,6 +36,9 @@ public class AccessTokenApi extends BaseAbstractApi {
                     wxResponseEntity.setBody(responseEntity.getBody());
                     log.info("getAccessToken 返回值为{}",responseEntity.getBody());
                 }).execute();
+        //可进行数据转换
+        ObjectMapper objectMapper = new ObjectMapper();
+        Map map = objectMapper.readValue(wxResponseEntity.getBody().toString(), HashMap.class);
         return wxResponseEntity;
     }
 }
