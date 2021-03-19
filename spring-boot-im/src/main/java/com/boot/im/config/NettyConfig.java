b5ff65ebc3760f27cd03d76097c438b6638cd25f
lizhifu <lizhifuabc@163.com>
2021-03-19 13:49:49 +0800

    Netty配置

--- spring-boot-im/src/main/java/com/boot/im/config/NettyConfig.java
+++ spring-boot-im/src/main/java/com/boot/im/config/NettyConfig.java
@@ -4,12 +4,16 @@ import com.boot.im.handler.ChatHandler;
 import com.boot.im.handler.HeartBeatHandler;
 import com.boot.im.handler.WSServerInitialzer;
 import io.netty.bootstrap.ServerBootstrap;
+import io.netty.channel.EventLoopGroup;
 import io.netty.channel.nio.NioEventLoopGroup;
 import io.netty.channel.socket.nio.NioServerSocketChannel;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnClass;
 import org.springframework.boot.autoconfigure.condition.ConditionalOnMissingBean;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+
+import javax.annotation.PreDestroy;
 
 
 /**
@@ -17,30 +21,21 @@ import org.springframework.context.annotation.Configuration;
  * @author lizhifu
  */
 @Configuration
-@ConditionalOnClass({NioEventLoopGroup.class,ServerBootstrap.class,
+@Slf4j
+@ConditionalOnClass({ServerBootstrap.class,
         WSServerInitialzer.class})
 public class NettyConfig {
+    /**
+     * 一个主线程组
+     * 用于接收客户端的链接，但不做任何处理
+     */
+    private EventLoopGroup bossGroup = new NioEventLoopGroup(1);
+    /**
+     * N个工作线程组
+     * 从线程组，主线程组会把任务转给从线程组进行处理
+     */
+    private EventLoopGroup workGroup = new NioEventLoopGroup(512);
 
-    /**
-     * 主线程
-     * 用于接受客户端的连接，但是不做任何处理
-     * @return
-     */
-    @Bean("mainGroup")
-    @ConditionalOnMissingBean
-    public NioEventLoopGroup mainGroup() {
-        return new NioEventLoopGroup();
-    }
-    /**
-     * 从线程
-     * mainGroup会将任务丢给他，让workerGroup去处理
-     * @return
-     */
-    @Bean("workerGroup")
-    @ConditionalOnMissingBean
-    public NioEventLoopGroup workerGroup() {
-        return new NioEventLoopGroup();
-    }
     @Bean("chanelInit")
     @ConditionalOnMissingBean
     public WSServerInitialzer wsServerInitialzer() {
@@ -54,16 +49,26 @@ public class NettyConfig {
     public ChatHandler chatHandler() {
         return new ChatHandler();
     }
+
     @Bean("server")
     @ConditionalOnMissingBean
-    public ServerBootstrap server(NioEventLoopGroup mainGroup,NioEventLoopGroup workerGroup,
-                                  WSServerInitialzer chanelInit) {
+    public ServerBootstrap server(WSServerInitialzer chanelInit) {
         return new ServerBootstrap()
                 //设置主从线程组
-                .group(mainGroup, workerGroup)
+                .group(bossGroup, workGroup)
                 // 设置nio的双向通道
                 .channel(NioServerSocketChannel.class)
-                //子处理器，用于处理workerGroup
+                //设置chanel初始化器
+                //每一个chanel由多个handler共同组成管道(pipeline)
                 .childHandler(chanelInit);
+    }
+    /**
+     * 关闭netty服务
+     */
+    @PreDestroy
+    public void destroy() {
+        bossGroup.shutdownGracefully().syncUninterruptibly();
+        workGroup.shutdownGracefully().syncUninterruptibly();
+        log.info("关闭 Netty 成功");
     }
 }
