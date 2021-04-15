9556543ceccdb5ac4957f9f5709e174d323b7d16
lizhifu <lizhifuabc@163.com>
2021-04-15 16:23:33 +0800

    Document crud

--- spring-boot-elasticsearch/README.md
+++ spring-boot-elasticsearch/README.md
@@ -66,5 +66,32 @@ lizhifudeMBP:bin lizhifu$ ./elasticsearch
 }
 ```
 
-# 实体映射
+# 索引相关操作
+
+```java
+/**
+ * 索引相关操作
+ *
+ * @author lizhifu
+ * @date 2021/4/15
+ */
+@SpringBootTest
+public class IndexApiTest {
+    @Resource
+    private ElasticsearchRestTemplate template;
+    /**
+     * 索引相关操作
+     */
+    @Test
+    public void index() {
+        IndexOperations indexOperations = template.indexOps(User.class);
+        System.out.println("索引是否存在：" + indexOperations.exists());
+        System.out.println("索引是否删除：" + indexOperations.delete());
+        System.out.println("索引创建结果：" + indexOperations.create());
+        System.out.println("映射修改结果：" + indexOperations.putMapping());
+    }
+}
+```
+
+> 映射修改结果需要安装相应版本的分词器
 
--- spring-boot-elasticsearch/src/test/java/com/boot/elasticsearch/DocumentApiTest.java
+++ spring-boot-elasticsearch/src/test/java/com/boot/elasticsearch/DocumentApiTest.java
@@ -21,43 +21,28 @@ public class DocumentApiTest {
     @Resource
     private ElasticsearchRestTemplate template;
     @Test
-    void test() {
-        saveOne();
-        getById();
-        deleteById();
+    public void test() {
+        User user = new User(1, "赵一", 18, "北京市昌平区", LocalDateTime.now());
+        template.save(user);
+        System.out.println("查询结果为："+template.get("1", User.class));
+        System.out.println("删除结果为："+template.delete("1", User.class));
         saveList();
     }
-
-    void saveOne() {
-        User user = new User(1, "张三", 18, "上海市闵行区", LocalDateTime.now());
-        template.save(user);
-    }
-
-    void getById() {
-        User user = template.get("1", User.class);
-        System.out.println(user);
-    }
-
-    void deleteById() {
-        String delete = template.delete("1", User.class);
-        System.out.println(delete);
-    }
-
-    void saveList() {
+    public void saveList() {
         List<User> userList = new ArrayList<>();
-        userList.add(new User(1, "Max", 16, "上海市闵行区", LocalDateTime.now()));
-        userList.add(new User(2, "Vicky", 17, "上海市长宁区", LocalDateTime.now()));
-        userList.add(new User(3, "张三", 18, "上海张江", LocalDateTime.now()));
-        userList.add(new User(4, "李四", 19, "江苏省盐城市", LocalDateTime.now()));
-        userList.add(new User(5, "王五", 20, "江苏省苏州斯", LocalDateTime.now()));
-        userList.add(new User(6, "赵六", 21, "江苏省南京市", LocalDateTime.now()));
-        userList.add(new User(7, "孙七", 22, "北京市朝阳区", LocalDateTime.now()));
-        userList.add(new User(8, "周八", 23, "山东济南", LocalDateTime.now()));
-        userList.add(new User(9, "吴九", 24, "河南郑州", LocalDateTime.now()));
-        userList.add(new User(10, "郑十", 25, "四川重庆", LocalDateTime.now()));
+        userList.add(new User(1, "赵1", 16, "北京市昌平区", LocalDateTime.now()));
+        userList.add(new User(2, "钱2", 17, "北京市朝阳区", LocalDateTime.now()));
+        userList.add(new User(3, "孙3", 18, "北京海淀", LocalDateTime.now()));
+        userList.add(new User(4, "李4", 19, "江苏省盐城市", LocalDateTime.now()));
+        userList.add(new User(5, "周5", 20, "江苏省苏州斯", LocalDateTime.now()));
+        userList.add(new User(6, "吴6", 21, "江苏省南京市", LocalDateTime.now()));
+        userList.add(new User(7, "郑7", 22, "北京市朝阳区", LocalDateTime.now()));
+        userList.add(new User(8, "王8", 23, "山东济南", LocalDateTime.now()));
+        userList.add(new User(9, "杨9", 24, "河南郑州", LocalDateTime.now()));
+        userList.add(new User(10, "钟10", 25, "四川重庆", LocalDateTime.now()));
         Iterable<User> users = template.save(userList);
         for (User user : users) {
-            System.out.println(user);
+            System.out.println("批量插入结果："+user);
         }
     }
 }
