47969948d1f32cca85c5156406f0ea6eb2825189
lizhifu <lizhifuabc@163.com>
2021-04-15 16:15:47 +0800

    索引相关操作

--- spring-boot-elasticsearch/src/test/java/com/boot/elasticsearch/IndexApiTest.java
+++ spring-boot-elasticsearch/src/test/java/com/boot/elasticsearch/IndexApiTest.java
@@ -9,7 +9,7 @@ import org.springframework.data.elasticsearch.core.IndexOperations;
 import javax.annotation.Resource;
 
 /**
- * IndexApi
+ * 索引相关操作
  *
  * @author lizhifu
  * @date 2021/4/15
@@ -19,34 +19,14 @@ public class IndexApiTest {
     @Resource
     private ElasticsearchRestTemplate template;
     /**
-     * 创建索引
+     * 索引相关操作
      */
     @Test
-    void createIndex() {
+    public void index() {
         IndexOperations indexOperations = template.indexOps(User.class);
-        exists(indexOperations);
-        delete(indexOperations);
-        create(indexOperations);
-        mapping(indexOperations);
-    }
-
-    void exists(IndexOperations indexOperations) {
-        boolean exists = indexOperations.exists();
-        System.out.println("索引是否存在：" + exists);
-    }
-
-    void delete(IndexOperations indexOperations) {
-        boolean delete = indexOperations.delete();
-        System.out.println("索引是否删除：" + delete);
-    }
-
-    void create(IndexOperations indexOperations) {
-        boolean flag = indexOperations.create();
-        System.out.println("索引创建结果：" + flag);
-    }
-
-    void mapping(IndexOperations indexOperations) {
-        boolean mapping = indexOperations.putMapping();
-        System.out.println("映射修改结果：" + mapping);
+        System.out.println("索引是否存在：" + indexOperations.exists());
+        System.out.println("索引是否删除：" + indexOperations.delete());
+        System.out.println("索引创建结果：" + indexOperations.create());
+        System.out.println("映射修改结果：" + indexOperations.putMapping());
     }
 }
