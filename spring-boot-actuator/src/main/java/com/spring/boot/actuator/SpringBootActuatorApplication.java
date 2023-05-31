1dd41ebb76d9f1842ff1ec0609290a8f650be18c
拔土豆的程序员 <lizhifuabc@163.com>
2023-05-31 23:51:20 +0800

    [新增] spring-boot-actuator

---
+++ spring-boot-actuator/src/main/java/com/spring/boot/actuator/SpringBootActuatorApplication.java
@@ -0,0 +1,17 @@
+package com.spring.boot.actuator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author lizhifu
+ * @since 2023/5/31
+ */
+@SpringBootApplication
+public class SpringBootActuatorApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootActuatorApplication.class, args);
+    }
+}
---
+++ spring-boot-web/src/main/java/com/spring/boot/web/SpringBootWebApplication.java
@@ -0,0 +1,17 @@
+package com.spring.boot.web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动类
+ *
+ * @author lizhifu
+ * @since 2023/5/31
+ */
+@SpringBootApplication
+public class SpringBootWebApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootWebApplication.class, args);
+    }
+}
