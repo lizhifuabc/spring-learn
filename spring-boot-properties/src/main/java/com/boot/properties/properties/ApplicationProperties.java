6494af6a9be31932453d40bd322fa99cfe12c3c9
拔土豆的程序员 <lizhifuabc@163.com>
2023-07-17 22:22:10 +0800

    [新增] spring boot 3 三种多环境配置文件方式

--- spring-boot-properties/src/main/java/com/boot/properties/properties/ApplicationProperties.java
+++ spring-boot-properties/src/main/java/com/boot/properties/properties/ApplicationProperties.java
@@ -1,5 +1,6 @@
 package com.boot.properties.properties;
 
+import lombok.Data;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -8,20 +9,12 @@ import org.springframework.stereotype.Component;
  * @author lizhifu
  */
 @Component
+@Data
 public class ApplicationProperties {
     @Value("${application.name}")
     private String name;
     @Value("${application.version}")
     private String version;
-
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
     @Override
     public String toString() {
         return "ApplicationProperties{" +
