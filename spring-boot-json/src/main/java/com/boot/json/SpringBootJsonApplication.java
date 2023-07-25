95ff5b91fd6f7ead5506a55bcd4a91df73204552
拔土豆的程序员 <lizhifuabc@163.com>
2023-07-25 17:32:21 +0800

    [新增] spring-boot-json

---
+++ spring-boot-json/src/main/java/com/boot/json/SpringBootJsonApplication.java
@@ -0,0 +1,16 @@
+package com.boot.json;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动程序
+ * @author lizhifu
+ * @since  2021/01/4
+ */
+@SpringBootApplication
+public class SpringBootJsonApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootJsonApplication.class);
+    }
+}
