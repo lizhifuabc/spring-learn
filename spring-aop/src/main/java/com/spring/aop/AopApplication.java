e405044224cd8b741eb9974365bfe67345d470d0
拔土豆的程序员 <lizhifuabc@163.com>
2023-09-17 22:35:22 +0800

    [新增] spring-aop

---
+++ spring-aop/src/main/java/com/spring/aop/AopApplication.java
@@ -0,0 +1,17 @@
+package com.spring.aop;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * AopApplication Aop测试
+ *
+ * @author lizhifu
+ * @since 2023/9/17
+ */
+@SpringBootApplication
+public class AopApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(AopApplication.class, args);
+    }
+}
