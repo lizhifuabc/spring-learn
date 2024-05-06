11629eb7d2d025f5993618ea12e4a34d32ea51fa
拔土豆的程序员 <lizhifuabc@163.com>
2024-05-06 18:04:11 +0800

    [新增] arthas-docker

---
+++ arthas-docker/src/main/java/com/arthas/docker/ArthasDockerApplication.java
@@ -0,0 +1,13 @@
+package com.arthas.docker;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArthasDockerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ArthasDockerApplication.class, args);
+	}
+
+}
