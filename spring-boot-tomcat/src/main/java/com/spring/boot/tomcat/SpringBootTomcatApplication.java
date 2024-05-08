4b9dd9735103fb4c8507c58dd6da7e47c6a85904
拔土豆的程序员 <lizhifuabc@163.com>
2024-05-08 15:59:42 +0800

    [新增] spring boot tomcat 请求配置

---
+++ spring-boot-tomcat/src/main/java/com/spring/boot/tomcat/SpringBootTomcatApplication.java
@@ -0,0 +1,16 @@
+package com.spring.boot.tomcat;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * 启动程序
+ * @author lizhifu
+ * @date 2021/1/18
+ */
+@SpringBootApplication
+public class SpringBootTomcatApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootTomcatApplication.class);
+    }
+}
